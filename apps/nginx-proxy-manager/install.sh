641b7df1a8faf8a2fa00db201d66379ff0bb2dcd
夏阳 <jiajianchang@outlook.com>
2024-09-22 00:18:54 +0800

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "v0.0.1";
+
 EPS_BASE_URL=${EPS_BASE_URL:-}
 EPS_OS_DISTRO=${EPS_OS_DISTRO:-}
 EPS_UTILS_COMMON=${EPS_UTILS_COMMON:-}
@@ -45,8 +46,74 @@ if [ -f "$EPS_SERVICE_FILE" ]; then
     /opt/certbot/bin/certbot
 fi
 
+
+step_start "Nginx Proxy Manager" "Downloading" "Downloaded"
+  NPM_VERSION=$(os_fetch -O- https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  os_fetch -O- https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v$NPM_VERSION | tar -xz
+  cd ./nginx-proxy-manager-$NPM_VERSION
+  step_end "Nginx Proxy Manager ${CLR_CYB}v$NPM_VERSION${CLR} ${CLR_GN}Downloaded"
+
+step_start "Enviroment" "Setting up" "Setup"
+  # Update NPM version in package.json files
+  sed -i "s/\"version\": \"0.0.0\"/\"version\": \"$NPM_VERSION\"/" backend/package.json
+  sed -i "s/\"version\": \"0.0.0\"/\"version\": \"$NPM_VERSION\"/" frontend/package.json
+  
+  # Fix nginx config files for use with openresty defaults
+  sed -i 's/user npm/user root/g; s/^pid/#pid/g; s+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
+  sed -i 's/include-system-site-packages = false/include-system-site-packages = true/g' /opt/certbot/pyvenv.cfg
+  _nginxConfigs=$(find ./ -type f -name "*.conf")
+  for _nginxConfig in $_nginxConfigs; do
+    sed -i 's+include conf.d+include /etc/nginx/conf.d+g' "$_nginxConfig"
+  done
+
+  # Copy runtime files
+  mkdir -p /var/www/html /etc/nginx/logs
+  cp -r docker/rootfs/var/www/html/* /var/www/html/
+  cp -r docker/rootfs/etc/nginx/* /etc/nginx/
+  cp docker/rootfs/etc/letsencrypt.ini /etc/letsencrypt.ini
+  cp docker/rootfs/etc/logrotate.d/nginx-proxy-manager /etc/logrotate.d/nginx-proxy-manager
+  ln -sf /etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
+  rm -f /etc/nginx/conf.d/dev.conf
+
+  # Create required folders
+  mkdir -p \
+	/data/nginx \
+	/data/custom_ssl \
+	/data/logs \
+	/data/access \
+	/data/nginx/default_host \
+	/data/nginx/default_www \
+	/data/nginx/proxy_host \
+	/data/nginx/redirection_host \
+	/data/nginx/stream \
+	/data/nginx/dead_host \
+	/data/nginx/temp \
+	/data/letsencrypt-acme-challenge \
+	/run/nginx \
+	/tmp/nginx/body \
+	/var/log/nginx \
+	/var/lib/nginx/cache/public \
+	/var/lib/nginx/cache/private \
+	/var/cache/nginx/proxy_temp
+
+  # Set permissions
+  touch /var/log/nginx/error.log
+  chmod 777 /var/log/nginx/error.log
+  chmod -R 777 /var/cache/nginx
+  chmod 644 /etc/logrotate.d/nginx-proxy-manager
+  chown root /tmp/nginx
+  chmod -R 777 /var/cache/nginx
+
+  # Dynamically generate resolvers file, if resolver is IPv6, enclose in `[]`
+  echo resolver "$(awk 'BEGIN{ORS=" "} $1=="nameserver" { sub(/%.*$/,"",$2); print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf) valid=10s;" > /etc/nginx/conf.d/include/resolvers.conf
+
+  # Copy app files
+  mkdir -p /app/global /app/frontend/images
+  cp -r backend/* /app
+  cp -r global/* /app/global
+
 step_start "Frontend" "Building" "Built"
-  cd /app/frontend
+  cd ./frontend
   export NODE_ENV=development
   yarn cache clean --silent --force >$__OUTPUT
   yarn install --silent --network-timeout=30000 >$__OUTPUT 
